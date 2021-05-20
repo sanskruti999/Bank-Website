4d9bfd22c511d62bb1e2d0ca3860ba080bde8920
sanskruti999 <65884165+sanskruti999@users.noreply.github.com>
2021-05-20 09:08:51 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,84 @@
+<?php       
+ 
+      //Database Connection 
+  $conn=mysqli_connect('localhost','Sanskruti','9075174720','bank_system');
+  if($conn){
+    echo " ";
+    }else{
+        echo "no connection";
+    }
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
